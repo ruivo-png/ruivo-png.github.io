978b12d6c6c5dcb3522ed3b2dc136f574eee6017
ruivo-png <56525096+ruivo-png@users.noreply.github.com>
2019-10-15 04:28:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+var widthMAX = window.innerWidth - 30,		// largura da parte de dentro da tela do navegador
+	heightMAX = window.innerHeight - 30;		// altura da parte de dentro da tela do navegador
 
+
+
+
+window.onload = function() {
+	
+	
+	
+}
+
+function ampliar (i) {
+	document.getElementById("coat").style.zIndex = "100";
+	document.getElementById("coat").style.opacity = "1";
+	document.getElementById("X").style.opacity = "1";
+	
+	document.getElementById("imagemampliada").height = heightMAX;
+	
+	if (i == "narancia") 
+		document.getElementById("imagemampliada").src = "illusts/narancia.png";
+	if (i == "mother3") 
+		document.getElementById("imagemampliada").src = "illusts/mother3.png";
+	if (i == "holymother") 
+		document.getElementById("imagemampliada").src = "illusts/holymother.jpg";
+}
+
+function fechar (){
+	document.getElementById("coat").style.zIndex = "0";
+	document.getElementById("coat").style.opacity = "0";
+	document.getElementById("X").style.opacity = "0";
+}
