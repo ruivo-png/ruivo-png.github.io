8231080caa6185ab6cef30662b691f9bf86668e4
ruivo-png <56525096+ruivo-png@users.noreply.github.com>
2019-10-16 23:56:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,76 @@
+
+	// !!!!!!!!!!!!!!!!
+	// BIBLIOTECA
+	// !!!!!!!!!!!!!!!!
+
+		// DESCRIÇÕES
+			
+				// fanart
+				
+					var desc_narancia_popart			= "Fanart from anime Jojo's Bizarre Adventure: Vento Aureo. Pop Art style.<br>First of a series of prints.";
+					var desc_mother3_photography		= "dskjsdlkfjLTESTEEEE";
+					var desc_mob_flutuando				= "dskjsdlsadfasdfsdfsdfsdfsESTEEEE";
+				
+				// original
+				
+					var desc_holymother					= "dskfdgdfgfgsgdfsESTEEEE";
+				
+				
+				
+		// THUMBNAILS
+			
+				// fanart
+				
+					var thumb_narancia_popart			= "url('thumbs/thumb_narancia_popart.png')";
+					var thumb_mother3_photography 		= "url('thumbs/thumb_mother3_photography.png')";
+					var thumb_mob_flutuando				= "url('thumbs/thumb_mob_flutuando.png')";
+					
+				// original
+				
+					var thumb_holymother				= "url('thumbs/thumb_holymother.png')";
+				
+				
+			
+		// IMAGENS AMPLIADAS
+			
+				// fanart
+				
+					var full_narancia_popart				= "illusts/narancia_popart.png";
+					var full_mother3_photography			= "illusts/mother3_photography.png";
+					var full_mob_flutuando					= "illusts/mob_flutuando.png";
+					
+				// original
+				
+					var full_holymother						= "illusts/holymother.jpg";
+				
+	
+	// !!!!!!!!!!!!!!!!
+	// ORDEM DAS ILUSTRAÇÕES
+	// !!!!!!!!!!!!!!!!
+	
+	
+		// thumbnails
+		
+			var thumb01 = thumb_narancia_popart,
+				thumb02 = thumb_mother3_photography,
+				thumb03 = thumb_mob_flutuando,
+				thumb04 = thumb_holymother,
+				thumb05;
+		
+	
+	
+		// imagens ampliadas
+		
+			var	full01 = full_narancia_popart,
+				full02 = full_mother3_photography,
+				full03 = full_mob_flutuando,
+				full04 = full_holymother,
+				full05;
+				
+
+				
+			var desc01 = desc_narancia_popart,
+				desc02 = desc_mother3_photography,
+				desc03 = desc_mob_flutuando,
+				desc04 = desc_holymother,
+				desc05;
