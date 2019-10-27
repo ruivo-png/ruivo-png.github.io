0157762ec697aaa8fa0d29f8c70fbe6dda5b9ea0
ruivo-png <56525096+ruivo-png@users.noreply.github.com>
2019-10-27 02:58:36 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,34 +3,194 @@ var widthMAX = window.innerWidth,		// largura da parte de dentro da tela do nave
 
 var previousBGColor = String(document.body.style.backgroundColor),
 	previousBG = String(document.body.style.backgroundImage);
-
-window.onload = function() {
 	
-	
-	thumbnails();
-}
+var lang;
 
-
-function thumbnails() {
-	document.getElementsByClassName("thumbnailimage")[0].style.backgroundImage = thumb01;
-	document.getElementsByClassName("thumbnailimage")[1].style.backgroundImage = thumb02;
-	document.getElementsByClassName("thumbnailimage")[2].style.backgroundImage = thumb03;
-	document.getElementsByClassName("thumbnailimage")[3].style.backgroundImage = thumb04;
-}
-
-
-function desc(desenho) {
-		if (desenho == '01')
-			document.getElementsByClassName("desc")[0].innerHTML = desc01;
-		if (desenho == '02')
-			document.getElementsByClassName("desc")[1].innerHTML = desc02;
-		if (desenho == '03')
-			document.getElementsByClassName("desc")[2].innerHTML = desc03;
-		if (desenho == '04')
-			document.getElementsByClassName("desc")[3].innerHTML = desc04;
+function currentPage(i, language) {
+	if (i == 'index') {
+		document.getElementById("about_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "none";
+		document.getElementById("design_container").style.display = "none";
+		document.getElementById("index_container").style.display = "flex";
+		
+		document.body.style.overflowY = "hidden";
 	}
+	
+	if (i == 'about') {
+		document.getElementById("index_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "none";
+		document.getElementById("design_container").style.display = "none";
+		document.getElementById("about_container").style.display = "flex";
+		
+		document.body.style.overflowY = "hidden";
+	}
+	
+	if (i == 'illustration') {
+		document.getElementById("about_container").style.display = "none";
+		document.getElementById("index_container").style.display = "none";
+		document.getElementById("design_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "inline";
+		
+		illustTab('original')
+		thumbnails_illust();
+		document.getElementById("button_orig").disabled = true;
+		document.getElementById("button_fan").disabled = false;
+		
+		document.body.style.overflowY = "scroll";
+	}
+	
+	if (i == 'design') {
+		document.getElementById("about_container").style.display = "none";
+		document.getElementById("index_container").style.display = "none";
+		document.getElementById("illustration_container").style.display = "none";
+		document.getElementById("design_container").style.display = "inline";
+	
+		thumbnails_illust();
+		
+		document.body.style.overflowY = "scroll";
+	}
+	
+	
+	
+	if (language != 0 && language != 1) {
+		translate(language);
+		document.getElementById("eng").disabled = true;
+		document.getElementById("ptbr").disabled = false;
+	}
+}
 
-function ampliar (i) {
+function illustTab(i) {
+	if (i == 'original') {
+		document.getElementById("fanart").style.display = "none";
+		document.getElementById("original").style.display = "inline";
+		document.getElementById("button_orig").disabled = true;
+		document.getElementById("button_fan").disabled = false;
+	}
+	
+	if (i == 'fanart') {
+		document.getElementById("original").style.display = "none";
+		document.getElementById("fanart").style.display = "inline";
+		document.getElementById("button_fan").disabled = true;
+		document.getElementById("button_orig").disabled = false;
+	}
+}
+
+function changeLang(language) {
+	translate(language);
+	
+	if (language == 0) {
+		document.getElementById("eng").disabled = true;
+		document.getElementById("ptbr").disabled = false;
+	}
+	
+	if (language == 1) {
+		document.getElementById("ptbr").disabled = true;
+		document.getElementById("eng").disabled = false;
+	}
+}
+
+function translate(language) {
+	if (language != 0 && language != 1)
+		lang = 0;
+	else {
+		lang = language;
+	}
+	
+	// INDEX
+	if (lang == 0)	{
+		document.getElementById("txt_intro").innerHTML = index[0];
+		document.getElementById("txt_top_lang").innerHTML = index[2];
+	}
+		
+	if (lang == 1)	{
+		document.getElementById("txt_intro").innerHTML = index[1];
+		document.getElementById("txt_top_lang").innerHTML = index[3];
+	}
+	
+	// ABOUT
+	
+	if (lang == 0)	{
+		document.getElementById("txt_top_about").innerHTML = about[2];
+		document.getElementById("txt_back_about").innerHTML = about[4];
+	}
+		
+	if (lang == 1)	{
+		document.getElementById("txt_top_about").innerHTML = about[3];
+		document.getElementById("txt_back_about").innerHTML = about[5];
+	}
+	
+	// ILLUST
+	if (lang == 0)	{
+		document.getElementById("txt_top_illust").innerHTML = illust[0];
+		document.getElementById("txt_back_illust").innerHTML = illust[2];
+	}
+		
+	if (lang == 1)	{
+		document.getElementById("txt_top_illust").innerHTML = illust[1];
+		document.getElementById("txt_back_illust").innerHTML = illust[3];
+	}
+	
+	// DESIGN
+	if (lang == 0)	{
+		document.getElementById("txt_back_design").innerHTML = design[0];
+	}
+		
+	if (lang == 1)	{
+		document.getElementById("txt_back_design").innerHTML = design[1];
+	}
+	
+	// ABOUT
+	if (lang == 0)	{
+		document.getElementById("txt_actualabout").innerHTML = about[0];
+	}
+		
+	if (lang == 1)	{
+		document.getElementById("txt_actualabout").innerHTML = about[1];
+	}
+}	
+
+function thumbnails_illust() {
+	var n;
+	
+	//original
+	for (n = 0; n < max_orig; n++)
+		document.getElementsByClassName("thumbnailimage_orig")[n].style.backgroundImage = orig_thumb[n];
+	
+	// fanart
+	for (n = 0; n < max_fan; n++)
+		document.getElementsByClassName("thumbnailimage_fan")[n].style.backgroundImage = fan_thumb[n];
+}
+
+function desc(tipo, i) {
+	if (tipo == "orig") {
+		document.getElementsByClassName("thumbnailcoat_orig")[i].style.background = "rgba(0, 0, 0, 0.8)";
+		document.getElementsByClassName("thumbnailimage_orig")[i].style.opacity = "0.2";
+		document.getElementsByClassName("orig_desc")[i].style.opacity = "1";
+		document.getElementsByClassName("orig_desc")[i].innerHTML = orig_desc[i];
+	}
+	if (tipo == "fan") {
+		document.getElementsByClassName("thumbnailcoat_fan")[i].style.background = "rgba(0, 0, 0, 0.8)";
+		document.getElementsByClassName("thumbnailimage_fan")[i].style.opacity = "0.2";
+		document.getElementsByClassName("fan_desc")[i].style.opacity = "1";
+		document.getElementsByClassName("fan_desc")[i].innerHTML = fan_desc[i];
+	}
+}
+
+function outdesc(tipo, i) {
+	
+	if (tipo == "orig") {
+		document.getElementsByClassName("thumbnailcoat_orig")[i].style.background = "rgba(0, 0, 0, 0)";
+		document.getElementsByClassName("thumbnailimage_orig")[i].style.opacity = "1";
+		document.getElementsByClassName("orig_desc")[i].style.opacity = "0";
+	}
+	if (tipo == "fan") {
+		document.getElementsByClassName("thumbnailcoat_fan")[i].style.background = "rgba(0, 0, 0, 0)";
+		document.getElementsByClassName("thumbnailimage_fan")[i].style.opacity = "1";
+		document.getElementsByClassName("fan_desc")[i].style.opacity = "0";
+	}
+}
+
+function ampliar (tipo, i) {
 	document.getElementById("coat").style.width = "100%";
 	document.getElementById("coat").style.height = "100%";
 	
@@ -49,14 +209,13 @@ function ampliar (i) {
 	document.getElementById("X").style.opacity = "1";
 	
 	
-	if (i == "01") 
-		document.getElementById("imagemampliada").src = full01;
-	if (i == "02") 
-		document.getElementById("imagemampliada").src = full02;
-	if (i == "03") 
-		document.getElementById("imagemampliada").src = full03;
-	if (i == "04") 
-		document.getElementById("imagemampliada").src = full04;
+	if (tipo == "orig")
+		document.getElementById("imagemampliada").src = orig_full[i];
+	
+	if (tipo == "fan")
+		document.getElementById("imagemampliada").src = fan_full[i];
+	
+	
 }
 
 function fechar (){
@@ -72,3 +231,8 @@ function fechar (){
 	document.getElementById("imagemampliada").src = "";
 }
 
+function treta() {
+	
+	getElementsByClassName("tretap")[0].innerHTML = "<div class = 'treta'><div class = 'abouttop'><div style = 'padding-top: 2px; float: left;'>///</div><div style = 'float: right; margin-right: 12px; margin-top: 5px;'><img src = 'img/close.png' style = 'float: right; margin-left: 2px;' /><img src = 'img/maximize.png' style = 'float: right;' /><img src = 'img/minimize.png' style = 'float: right;' /></div></div><br>aaaaaaaaa</div>";
+	
+}
--- dados.js
+++ dados.js
@@ -1,5 +1,96 @@
 
 	// !!!!!!!!!!!!!!!!
+	// ABOUT
+	// !!!!!!!!!!!!!!!!
+
+		// index
+
+			var index = [];
+				
+						// intro
+				
+							// english
+								index[0] = "Rio de Janeiro based artist. Welcome to my portfolio!<br>Apologies - 'Design' page currently only available in portuguese.";
+							// portuguese
+								index[1] = "Artista carioca. Bem vindo ao meu portfólio!";
+	
+		// about
+		
+			var about = [];
+	
+						// actual about
+						
+							// english
+								about[0] = "Hey there, <b style = 'color: #2781B3;'>Gabriella Ruivo</b> here!<br><br>I'm a Brazilian Digital Media Design student at university PUC-Rio, currently in my 4th semester.<br><br>Forever drowned in <b>curiosity</b>, I consider myself an interdisciplinary kinda person. My heart beats both for art and technology, but though i seek to specialize in illustration and animation, I make a point to not limit my fascination to any field, and like to see them connecting. The world is too big and interesting!<br><br>This means I also have a <b>wide range of tastes</b>: my interests go from wholesome children's stories to the occasional psychological horror. I also have a thing for glitch art and old-school tech aesthetics, as it shows in this 90s/synthwave-esque page I've coded.<br><br>I strive to show my passion through my work, giving it my all, and am very adaptable. If you have any nice ideas, let's talk!";
+							// portuguese
+								about[1] = "Hey, <b style = 'color: #2781B3;'>Gabriella Ruivo</b> aqui!<br><br>Sou estudante de Design de Mídia Digital na PUC-Rio, atualmente no meu quarto semestre.<br><br>Sempre afogada em <b>curiosidade</b>, eu me considero uma pessoa interdisciplinar. Meu coração bate para arte e tecnologia, e embora eu procure me especializar em ilustração e animação, não limito minha fascinação para nenhum campo, e gosto de vê-los se conectando. O mundo é grande e interessante demais!<br><br>Isso quer dizer que eu tenho uma <b>ampla gama de gostos</b>: meus interesses vão de histórias infantis até o ocasional horror psicológico. Também tenho algo por arte glitch e estéticas de tech retrô, como é possível ver nessa presente página anos 90/meio synthwave que codei.<br><br>Eu procuro mostrar minha paixão pelas coisas através do meu trabalho, dando meu melhor, e sou muito adaptável. Se você tiver alguma ideia, vamos trocar!";
+	
+	// !!!!!!!!!!!!!!!!
+	// AUTOMATIC TRANSLATIONS ENG & PTBR
+	// !!!!!!!!!!!!!!!!
+
+		// INDEX
+
+		
+				// top
+				
+					// english
+						index[2] = "languages.exe";
+					// portuguese
+						index[3] = "idioma.exe";
+						
+						
+				// links
+				
+					// english
+					
+					// portuguese
+						
+		// ILLUSTRATION
+		
+		
+		var illust = [];
+		
+				// top
+	
+					// english
+						illust[0] = "illustration.zip";
+					// portuguese
+						illust[1] = "ilustracao.zip";
+						
+				// botões
+				
+					// english
+						illust[2] = "Back";
+					// portuguese
+						illust[3] = "Voltar";
+
+		var design = [];
+		
+				// botões
+				
+					// english
+						design[0] = "Back";
+					// portuguese
+						design[1] = "Voltar";
+						
+		// ABOUT
+		
+				// top
+	
+					// english
+						about[2] = "about.txt";
+					// portuguese
+						about[3] = "sobre.txt";
+						
+				// botões
+					
+					// english
+						about[4] = "Back";
+					// portuguese
+						about[5] = "Voltar";
+
+	// !!!!!!!!!!!!!!!!
 	// BIBLIOTECA
 	// !!!!!!!!!!!!!!!!
 
@@ -7,17 +98,33 @@
 			
 				// fanart
 				
-					var desc_narancia_popart			= "Fanart from anime Jojo's Bizarre Adventure: Vento Aureo. Pop Art style.<br>First of a series of prints.";
-					var desc_mother3_photography		= "dskjsdlkfjLTESTEEEE";
-					var desc_mob_flutuando				= "dskjsdlsadfasdfsdfsdfsdfsESTEEEE";
+					// english
+						var desc_narancia_popart			= "<b>Pop Art Jojo</b><br>A5 print series<br>2019<br><br>[Other characters soon]";
+						var desc_mother3_photography		= "<b>MOTHER3</b><br>2019";
+						var desc_mob_flutuando				= "<b>MP100</b><br>2019";
+						
+					// portuguese
+						
 				
 				// original
 				
-					var desc_holymother					= "dskfdgdfgfgsgdfsESTEEEE";
-				
+					// english
+						var desc_holymother					= "<b>O Holy Mother</b><br>2018";
+						var desc_notanybetter				= "<b>Exhausted</b><br>2018";
+						var desc_bubbles					= "<b>Bubbles</b><br>2019";
+						var desc_projcadernos				= "<b>Notebooks project - cover</b><br>2019";
+						
+					// portuguese
 				
 				
 		// THUMBNAILS
+			
+				// original
+				
+					var thumb_holymother				= "url('thumbs/thumb_holymother.png')";
+					var thumb_notanybetter				= "url('thumbs/thumb_notanybetter.png')";
+					var thumb_bubbles					= "url('thumbs/thumb_bubbles.png')";
+					var thumb_projcadernos				= "url('thumbs/thumb_projcadernos.png')";
 			
 				// fanart
 				
@@ -25,13 +132,18 @@
 					var thumb_mother3_photography 		= "url('thumbs/thumb_mother3_photography.png')";
 					var thumb_mob_flutuando				= "url('thumbs/thumb_mob_flutuando.png')";
 					
-				// original
 				
-					var thumb_holymother				= "url('thumbs/thumb_holymother.png')";
 				
 				
 			
 		// IMAGENS AMPLIADAS
+			
+				// original
+				
+					var full_holymother						= "illusts/holymother.jpg";
+					var full_notanybetter					= "illusts/notanybetter.png";
+					var full_bubbles						= "illusts/bubbles.png";
+					var full_projcadernos					= "illusts/projcadernos.png";
 			
 				// fanart
 				
@@ -39,38 +151,76 @@
 					var full_mother3_photography			= "illusts/mother3_photography.png";
 					var full_mob_flutuando					= "illusts/mob_flutuando.png";
 					
-				// original
 				
-					var full_holymother						= "illusts/holymother.jpg";
 				
 	
 	// !!!!!!!!!!!!!!!!
-	// ORDEM DAS ILUSTRAÇÕES
+	// PAGE: ILLUST - ORDEM DAS ILUSTRAÇÕES
 	// !!!!!!!!!!!!!!!!
+	
+		var max_orig =	4,
+			max_fan	=	3;
 	
 	
 		// thumbnails
 		
-			var thumb01 = thumb_narancia_popart,
-				thumb02 = thumb_mother3_photography,
-				thumb03 = thumb_mob_flutuando,
-				thumb04 = thumb_holymother,
-				thumb05;
+			// original
+			
+				var orig_thumb = [];
+				
+				orig_thumb[0] = thumb_bubbles;
+				orig_thumb[1] = thumb_projcadernos;
+				orig_thumb[2] = thumb_holymother;
+				orig_thumb[3] = thumb_notanybetter;
 		
-	
+			// fanart
+		
+				var fan_thumb = [];
+				
+				fan_thumb[0] = thumb_narancia_popart;
+				fan_thumb[1] = thumb_mother3_photography;
+				fan_thumb[2] = thumb_mob_flutuando;
+				fan_thumb[3];
 	
 		// imagens ampliadas
 		
-			var	full01 = full_narancia_popart,
-				full02 = full_mother3_photography,
-				full03 = full_mob_flutuando,
-				full04 = full_holymother,
-				full05;
+			// original
+			
+				var orig_full = [];
 				
-
+				orig_full[0] = full_bubbles;
+				orig_full[1] = full_projcadernos;
+				orig_full[2] = full_holymother;
+				orig_full[3] = full_notanybetter;
 				
-			var desc01 = desc_narancia_popart,
-				desc02 = desc_mother3_photography,
-				desc03 = desc_mob_flutuando,
-				desc04 = desc_holymother,
-				desc05;
+			// fanart
+		
+				var fan_full = [];
+				
+				fan_full[0] = full_narancia_popart;
+				fan_full[1] = full_mother3_photography;
+				fan_full[2] = full_mob_flutuando;
+				fan_full[3];
+					
+		// descrições
+				
+			// original
+			
+				var orig_desc = [];
+				
+				orig_desc[0] = desc_bubbles;
+				orig_desc[1] = desc_projcadernos;
+				orig_desc[2] = desc_holymother;
+				orig_desc[3] = desc_notanybetter;
+			
+			// fanart
+			
+				var fan_desc = [];
+				
+				fan_desc[0] = desc_narancia_popart;
+				fan_desc[1] = desc_mother3_photography;
+				fan_desc[2] = desc_mob_flutuando;
+				fan_desc[3];
+				
+				
+	
